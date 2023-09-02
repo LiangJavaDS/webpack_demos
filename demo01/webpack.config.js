ed7e8028ee24605ef7ad7c0465074f54136b671c
lhy <lhy_web@163.com>
2023-09-02 15:07:08 +0800

    demo1，使用path指定打包文件路径 && 指定打包模式为production

--- demo01/webpack.config.js
+++ demo01/webpack.config.js
@@ -1,6 +1,10 @@
+const path = require('path');
+
 module.exports = {
   entry: './main.js',
   output: {
-    filename: 'bundle.js'
+    filename: 'bundle.js',
+    // 将当前文件所在的目录路径作为输出路径。__dirname是Node.js中的一个全局变量，表示当前文件所在的目录路径
+    path: path.resolve(__dirname)
   }
 };
