070e703a179bda032ff2b75084e975e6ac273df3
lhy <lhy_web@163.com>
2023-09-02 17:36:05 +0800

    demo04，使用loader识别js引入的css

--- demo04/webpack.config.js
+++ demo04/webpack.config.js
@@ -10,6 +10,8 @@ module.exports = {
     rules: [
       {
         test: /\.css$/,
+        // style-loader用于将css代码插入到HTML中的<style>标签中
+        // css-loader用于在js中通过require或者import引入css成功
         use: ['style-loader', 'css-loader']
       },
     ]
