b9f01d3fead7a712a8afd5f76673253c398da0c2
lhy <lhy_web@163.com>
2023-09-02 16:48:44 +0800

    使用babel-loader解析jsx以及实现polyfill

--- demo03/webpack.config.js
+++ demo03/webpack.config.js
@@ -14,7 +14,16 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['es2015', 'react']
+            // 旧版的Babel预设
+            // presets: ['es2015', 'react']
+            // 新版的Babel预设
+
+            // @babel/preset-env 可以根据目标环境自动确定需要的插件和 polyfill
+            // @babel/preset-react 则用于将 JSX 转换为 JavaScript。
+
+            // polyfill的意思：Polyfill 是一种技术，它可以在旧版浏览器或其他不支持某些新特性的环境中，模拟实现这些新特性。
+            // polyfill 会检测当前环境是否已经支持某个新特性，如果支持则不做处理，否则就加载相应的 polyfill 代码。
+            presets: ['@babel/preset-env', '@babel/preset-react']
           }
         }
       }
