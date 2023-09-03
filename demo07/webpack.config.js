5b5983d01a347a6e326e803c117d2c38d5c47fb1
lhy <lhy_web@163.com>
2023-09-03 10:45:10 +0800

    demo07_压缩代码体积

--- demo07/webpack.config.js
+++ demo07/webpack.config.js
@@ -1,6 +1,7 @@
 var webpack = require('webpack');
 var UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const path = require('path')
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
   mode: 'production',
@@ -12,7 +13,19 @@ module.exports = {
   devServer: {
     static: '.',
   },
-  plugins: [
-    new UglifyJsPlugin()
-  ]
+  // 压缩代码webpack插件
+  // plugins: [
+  //   new UglifyJsPlugin()
+  // ]
+
+  // webpack5自带的压缩插件
+  // 优化选项
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        // 配置选项
+      })
+    ]
+  }
 };
