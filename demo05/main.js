bcdc8b4919e328091ae54dac0f578c6f48ee11f4
lhy <lhy_web@163.com>
2023-09-03 09:16:41 +0800

    demo05完成图片引入

--- demo05/main.js
+++ demo05/main.js
@@ -1,7 +1,11 @@
+import small from './small.png'
+import big from './big.png'
+
 var img1 = document.createElement("img");
-img1.src = require("./small.png");
+img1.src = small;
 document.body.appendChild(img1);
 
 var img2 = document.createElement("img");
-img2.src = require("./big.png");
+img2.src = big;
 document.body.appendChild(img2);
+console.log('this is main.js')
--- demo05/webpack.config.js
+++ demo05/webpack.config.js
@@ -1,19 +1,30 @@
 const path = require('path')
 
 module.exports = {
+  mode: 'production',
   entry: './main.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname)
   },
+  devServer: {
+    static: '.',
+  },
   module: {
     rules: [
       {
         test: /\.(png|jpg)$/,
+
+        // type是 webpack 5 中的新特性 asset/resource，将所有匹配到的 PNG 和 JPG 图片文件打包成单独的文件，并输出到输出目录中。
+        // type: 'asset/resource',
+
+        // 将图片文件转成Base64编码的URL字符串，并在打包后的js文件中引入
         use: [
           {
+            // 使用此loader的限制是js文件使用import语法而不是require
             loader: 'url-loader',
             options: {
+              // limit的单位为byte，注意若图片大于limit则打包成文件，小于则转换为base64字符串
               limit: 8192
             }
           }
